c522ac140ad9e92388004d22aef4c45ff81e7501
thuvarakans <58292928+thuvarakans@users.noreply.github.com>
2021-04-01 12:02:37 +0530

    Update mainn.js

--- mainn.js
+++ mainn.js
@@ -3,6 +3,6 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('SW.js');
+             .register('https://github.com/thuvarakans/flowchart-and-sudocode/blob/master/SW.js');
   }
 }
